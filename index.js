e6bd55a73d773201c04b105ff9811f4bdaac434a
pavan485 <76582086+pavan485@users.noreply.github.com>
2022-04-07 00:07:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,21 +69,21 @@ async function postToNewRelic(response) {
 	*/
 	let newRelicJsonString = '[{"metrics":[]}]';
 	let newRelicJsonPayload = JSON.parse(newRelicJsonString);
-	processTestData(testId, nodeName, timestamp, response.Summary.Timing, newRelicJsonPayload[0].metrics);
+		processTestData(testId, nodeName, timestamp, response.Summary.Timing, newRelicJsonPayload[0].metrics);
 
 	if (response.Summary.hasOwnProperty('Error'))
-	processErrorTestData(testId, nodeName, timestamp, response.Summary.Error,  newRelicJsonPayload[0].metrics, params);
+		processErrorTestData(testId, nodeName, timestamp, response.Summary.Error,  newRelicJsonPayload[0].metrics, params);
 	
 	
 	if (response.Summary.hasOwnProperty('Byte'))
-	processTestData(testId, nodeName, timestamp, response.Summary.Byte.Response,  newRelicJsonPayload[0].metrics);
+		processTestData(testId, nodeName, timestamp, response.Summary.Byte.Response,  newRelicJsonPayload[0].metrics);
 
 	if (response.hasOwnProperty('TestDetail'))
-	processTestData(testId, nodeName, timestamp, response.TestDetail, newRelicJsonPayload[0].metrics);
+		processTestData(testId, nodeName, timestamp, response.TestDetail, newRelicJsonPayload[0].metrics);
 
-	if (response.Summary.Timing.hasOwnProperty('ContentType')) {
-	processTestData(testId, nodeName, timestamp, response.Summary.Timing.ContentType, newRelicJsonPayload[0].metrics);
-	}
+	if (response.Summary.Timing.hasOwnProperty('ContentType')) 
+		processTestData(testId, nodeName, timestamp, response.Summary.Timing.ContentType, newRelicJsonPayload[0].metrics);
+	
 
 	/** If test type is Traceroute then compute RTT, Packet Loss, #Hops.*/
 	if (response.TestDetail.TypeId === TRACEROUTE_TEST_ID) {
